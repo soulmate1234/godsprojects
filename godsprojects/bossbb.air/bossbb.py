2f7d0215e166c47db2ba05bffa1349267c60e0a6
shijifeng <853467358@qq.com>
2020-11-30 17:24:47 +0800

    sjf

--- godsprojects/bossbb.air/bossbb.py
+++ godsprojects/bossbb.air/bossbb.py
@@ -8,4 +8,7 @@ start_app("com.sxc.bossbb")
 touch(Template(r"tpl1605692021907.png", record_pos=(-0.047, -0.056), resolution=(720, 1440)))
 text("15757185534")
 touch(Template(r"tpl1605692046781.png", record_pos=(0.028, 0.14), resolution=(720, 1440)))
+
 text("12345")
+touch(Template(r"tpl1606706034159.png", record_pos=(-0.003, 0.312), resolution=(720, 1440)))
+touch(Template(r"tpl1606706046685.png", record_pos=(-0.001, 0.058), resolution=(720, 1440)))
